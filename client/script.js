575169b0ae8bde7b69f69ed341e843717438a569
Daniil <spartaq123456789@gmail.com>
2020-02-13 17:22:58 +0600

    change mechanics of moving elements + add moving to inactive and back

--- client/script.js
+++ client/script.js
@@ -1,41 +1,54 @@
-var form = document;
-var activeBox = form.getElementsByClassName("todobox__active")[0];
-var checksTodo = form.getElementsByClassName("todobox-element__button");
-var inactiveBox = form.getElementsByClassName("todobox__inactive")[0];
-var buttonAddElem = form.getElementById("adder__submit");
-var inputTextField = form.getElementById("adder__text");
+const doc = document;
+const wnd = window;
+const serverUrl = "http://localhost:3000";
 
-var url = "http://localhost:3000";
 
-//Add for all checkboxes hide-after-click property
-function addListeners(){
-for(let i = 0; i < checksTodo.length; i++){
-    checksTodo[i].addEventListener("click", function (elem) {
-        if(elem.srcElement.parentNode.parentNode != null){
-            inactiveBox.innerHTML += elem.srcElement.parentNode.outerHTML;
-            //Delete from active todobox todo element
-            elem.srcElement.parentNode.parentNode.removeChild(elem.srcElement.parentNode);
+let activeBox = doc.getElementsByClassName("todobox__active")[0];
+let inactiveBox = doc.getElementsByClassName("todobox__inactive")[0];
+let checks = doc.getElementsByClassName("todobox-element__button");
+let buttonAddElem = doc.getElementById("adder__submit");
+let inputTextField = doc.getElementById("adder__text");
+
+window.addEventListener("beforeunload", function(elem){
+    let inactiveTodoes = inactiveBox.childNodes;
+    for(let i = 1; i < inactiveTodoes.length; i++){
+        deleteTodoFromServer(inactiveTodoes[i].childNodes[0].innerHTML);
+    }
+})
+
+doc.addEventListener("click", function(elem){
+    let src = elem.srcElement;
+    let box = src.parentNode.parentNode;
+
+    if(src.className == "todobox-element__button"){
+        if(box.className == "todobox__active"){
+            inactiveBox.innerHTML += src.parentNode.outerHTML;
+            box.removeChild(src.parentNode);
         }
-        deleteTodoFromServer(elem.srcElement.parentNode.firstChild.innerHTML);
-    });
-};
-}
+        else if(box.className == "todobox__inactive"){
+            activeBox.innerHTML += src.parentNode.outerHTML;
+            box.removeChild(src.parentNode);
+        }
+    }
+
+    let inactiveTodoes = inactiveBox.childNodes;
+    for(let i = 1; i < inactiveTodoes.length; i++){
+        console.log(inactiveTodoes[i].childNodes[0].innerHTML);
+    }
+})
 
 //Create new TODO from text field
 buttonAddElem.addEventListener("click", function (event){
     postNewTodoToServer(inputTextField.value);
     activeBox.innerHTML += createNewTodo(inputTextField.value).outerHTML;
-    console.log("dwadwa");
-    addListeners();
 });
-
 
 function createNewTodo(text){
     let tmpText = text;
     inputTextField.value = "";
-    var newTodo = form.createElement("div");
-    var newTodoText = form.createElement("div");
-    var newTodoCheckbox = form.createElement("div");
+    var newTodo = doc.createElement("div");
+    var newTodoText = doc.createElement("div");
+    var newTodoCheckbox = doc.createElement("div");
     newTodo.className = "todobox-element"; 
     newTodoText.className = "todobox-element__text"; 
     newTodoCheckbox.className = "todobox-element__button";
@@ -46,13 +59,12 @@ function createNewTodo(text){
 
 function getAllTodoesFromServer(){
     var request = new XMLHttpRequest();
-    request.open("GET", url);
+    request.open("GET", serverUrl);
     request.onload = () => {
         let todoes = JSON.parse(request.response);
         for(let i = 0; i < todoes.length; i++){
             activeBox.innerHTML += createNewTodo(todoes[i].todo_text).outerHTML;
         }
-        addListeners();
     }
     request.send();
 }
@@ -60,7 +72,7 @@ getAllTodoesFromServer();
 
 function postNewTodoToServer(text){
     var xtr = new XMLHttpRequest();
-    xtr.open("POST", url + "/add_new_todo", true);
+    xtr.open("POST", serverUrl + "/add_new_todo", true);
     xtr.send(JSON.stringify({
         todo_text: text
     }));
@@ -68,7 +80,7 @@ function postNewTodoToServer(text){
 
 function deleteTodoFromServer(text){
     var xtr = new XMLHttpRequest();
-    xtr.open("POST", url + "/delete_an_todo", true);
+    xtr.open("POST", serverUrl + "/delete_an_todo", true);
     xtr.send(JSON.stringify({
         todo_text: text
     }));
